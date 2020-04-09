f98d58e23ea31b01146c70ee14f054e5ac55c179
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-04-09 16:40:48 +0100

    still trying to resolve chellenge 3

--- src/process.js
+++ src/process.js
@@ -60,7 +60,9 @@ const getDollarsInFlight = (data, impact) => {
   const InfectionsByRequestedTime = getInfectionsByRequestedTime(data, impact);
   const { avgDailyIncomeInUSD, avgDailyIncomePopulation } = data.region;
   const timeToElapse = periodNormaliser(data.periodType, data.timeToElapse);
-  return InfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse;
+  return Math.trunc(
+    InfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse
+  );
 };
 
 // This the main function that return the final output base on inputed data
