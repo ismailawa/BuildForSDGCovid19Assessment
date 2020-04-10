3418d747c321b56c170449c5c635e0ea34a54fa1
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-04-10 16:55:52 +0100

    local branch is working

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { routes } = require('./api/routes/estimator');
 
 const app = express();
+const port = process.env.PORT || '3000';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,5 +11,5 @@ app.use('/api/v1/on-covid-19', routes);
 
 app.get('/', (req, res) => (res.send({ home: 'This is the home page..' })));
 
-app.listen(3000, () => {
+app.listen(port, () => {
 });
